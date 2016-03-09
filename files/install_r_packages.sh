6752506e1ea35f5a709b78e8d116b03d1f33dc6d
Gerhard Weis <g.weis@griffith.edu.au>
2016-03-10 07:04:50 +1000

    use R to install packages

--- files/install_r_packages.sh
+++ files/install_r_packages.sh
@@ -1,164 +1,130 @@
-#!/bin/bash
-set -x
-set -e
-TMPDIR=/tmp/r_packages
-mkdir -p $TMPDIR
-mkdir -p /usr/share/doc/R-3.2.2/html/
+#! /usr/bin/env Rscript
 
-curl -o $TMPDIR/rjson_0.2.13.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/rjson/rjson_0.2.13.tar.gz
-R CMD INSTALL $TMPDIR/rjson_0.2.13.tar.gz
 
-curl -o $TMPDIR/sp_1.0-15.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/sp/sp_1.0-15.tar.gz
-R CMD INSTALL $TMPDIR/sp_1.0-15.tar.gz
+#TODO: requires yum install libcurl-devel openssl-devel (for devtools)
 
-curl -o $TMPDIR/raster_2.2-31.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/raster/raster_2.2-31.tar.gz
-R CMD INSTALL $TMPDIR/raster_2.2-31.tar.gz
 
-curl -o $TMPDIR/rgdal_0.8-16.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/rgdal/rgdal_0.8-16.tar.gz
-R CMD INSTALL $TMPDIR/rgdal_0.8-16.tar.gz
+# setup local mirror
+repos <- getOption('repos')
+repos['CRAN'] = 'http://cran.csiro.au'
+options(repos = repos)
 
-curl -o $TMPDIR/rgeos_0.3-4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/rgeos/rgeos_0.3-4.tar.gz
-R CMD INSTALL $TMPDIR/rgeos_0.3-4.tar.gz
+# require devtools for install_version command
+install.package('devtools')
+require('devtools')
 
-curl -o $TMPDIR/gam_1.09.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gam/gam_1.09.1.tar.gz
-R CMD INSTALL $TMPDIR/gam_1.09.1.tar.gz
+# define list of packages to install
+pkglist = c(
+    'FNN',          '1.1',
+    'KernSmooth',   '2.23-15',
+    'MASS',         '7.3-45',
+    'Matrix',       '1.2-3',
+    'R.methodsS3',  '1.7.1',
+    'R.oo',         '1.20.0',
+    'R.utils',      '2.2.0',
+    'R2HTML',       '2.3.1',
+    'RColorBrewer', '1.1-2',
+    'Rcpp',         '0.12.3',
+    'SDMTools',     '1.1-221',
+    'abind',        '1.4-3',
+    'biomod2',      '3.1-64',
+    'boot',         '1.3-17',
+    'class',        '7.3-14',
+    'cluster',      '2.0.3',
+    'codetools',    '0.2-14',
+    'colorspace',   '1.2-6',
+    'deldir',       '0.1-9',
+    'dichromat',    '2.0-0',
+    'digest',       '0.6.9',
+    'dismo',        '1.0-15',
+    'evaluate',     '0.8',
+    'foreach',      '1.4.3',
+    'foreign',      '0.8-66',
+    'gam',          '1.12',
+    'gamlss',       '4.3-8',
+    'gamlss.data',  '4.3-2',
+    'gamlss.dist',  '4.3-5',
+    'gbm',          '2.1.1',
+    'ggplot2',      '2.0.0',
+    'gridExtra',    '2.2.0',
+    'gstat',        '1.1-2',
+    'gtable',       '0.2.0',
+    'hexbin',       '1.27.1',
+    'intervals',    '0.15.1',
+    'iterators',    '1.0.8',
+    'labeling',     '0.3',
+    'lattice',      '0.20-33',
+    'latticeExtra', '0.6-28',
+    'magrittr',     '1.5',
+    'mda',          '0.4-8',
+    'mgcv',         '1.8-9',
+    'munsell',      '0.4.3',
+    'nlme',         '3.1-122',
+    'nnet',         '7.3-11',
+    'pROC',         '1.8',
+    'plyr',         '1.8.3',
+    'png',          '0.1-7',
+    'proto',        '0.3-10',
+    'randomForest', '4.6-12',
+    'raster',       '2.5-2',
+    'rasterVis',    '0.37',
+    'reshape',      '0.8.5',
+    'reshape2',     '1.4.1',
+    'rgdal',        '1.1-3',
+    'rgeos',        '0.3-17',
+    'rjson',        '0.2.15',
+    'rpart',        '4.1-10',
+    'scales',       '0.4.0',
+    'sp',           '1.2-2',
+    'spacetime',    '1.1-5',
+    'spatial',      '7.3-11',
+    'stringi',      '1.0-1',
+    'stringr',      '1.0.0',
+    'survival',     '2.38-3',
+    'xts',          '0.9-7',
+    'zoo',          '1.7-12'
+)
 
-curl -o $TMPDIR/Rcpp_0.11.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/Rcpp/Rcpp_0.11.1.tar.gz
-R CMD INSTALL $TMPDIR/Rcpp_0.11.1.tar.gz
+# install method
+install_package <- function(pkg) {
+    installed = installed.packages()
+    if (is.element(pkg['pkg'], installed[,1])) {
+        if (pkg['ver'] != installed[pkg['pkg'], 'Version']) {
+            # already installed
+            return;
+        }
+    }
+    install_version(package=pkg['pkg'], version=pkg['ver'])
+}
 
-curl -o $TMPDIR/plyr_1.8.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/plyr/plyr_1.8.1.tar.gz
-R CMD INSTALL $TMPDIR/plyr_1.8.1.tar.gz
+# create package matrix and install packages
+pkgnames = pkglist[seq(1, length(pkglist), 2)]
+packages = matrix(pkglist, ncol=2, nrow=length(pkgnames), byrow=TRUE,
+                  dimnames=list(pkgnames, c('pkg', 'ver')))
 
-curl -o $TMPDIR/pROC_1.7.2.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/pROC/pROC_1.7.2.tar.gz
-R CMD INSTALL $TMPDIR/pROC_1.7.2.tar.gz
+# apply install method to packages data frame
+apply(packages, 1, install_package)
 
-curl -o $TMPDIR/R2HTML_2.2.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/R2HTML/R2HTML_2.2.1.tar.gz
-R CMD INSTALL $TMPDIR/R2HTML_2.2.1.tar.gz
+# function to return list of installed package that are not part of base installation
+nobase_pkglist <- function() {
+    installed = installed.packages()
+    # build index vector to filter out base packages
+    nobase = as.vector(installed[,'Priority']) != 'base'
+    # replace possible NA values with TRUE (nobase)
+    nobase[is.na(nobase)] = TRUE
+    return(installed[nobase,])
+}
 
-curl -o $TMPDIR/png_0.1-7.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/png_0.1-7.tar.gz
-R CMD INSTALL $TMPDIR/png_0.1-7.tar.gz
+# get alphabetically sorted list of currently installed packages in a
+# format suitable to copy/paste here
+print_nobase_pkgs <- function() {
+    pkgs = nobase_pkglist()
+    pkgstrings = c()
+    for (name in sort(row.names(pkgs))) {
+        append(pkgstrings,
+               sprintf("    %-15s '%s'", paste("'", name , "',", sep=""), pkgs[name, 'Version']))
+    }
+    cat(paste(pkgstrings, sep=",\n"))
+}
 
-curl -o $TMPDIR/survival_2.37-7.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/survival/survival_2.37-7.tar.gz
-R CMD INSTALL $TMPDIR/survival_2.37-7.tar.gz
 
-curl -o $TMPDIR/lattice_0.20-29.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/lattice/lattice_0.20-29.tar.gz
-R CMD INSTALL $TMPDIR/lattice_0.20-29.tar.gz
-
-curl -o $TMPDIR/gbm_2.0-8.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gbm/gbm_2.0-8.tar.gz
-R CMD INSTALL $TMPDIR/gbm_2.0-8.tar.gz
-
-curl -o $TMPDIR/stringr_0.6.2.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/stringr/stringr_0.6.2.tar.gz
-R CMD INSTALL $TMPDIR/stringr_0.6.2.tar.gz
-
-curl -o $TMPDIR/evaluate_0.5.3.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/evaluate/evaluate_0.5.3.tar.gz
-R CMD INSTALL $TMPDIR/evaluate_0.5.3.tar.gz
-
-curl -o $TMPDIR/dismo_0.9-3.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/dismo/dismo_0.9-3.tar.gz
-R CMD INSTALL $TMPDIR/dismo_0.9-3.tar.gz
-
-curl -o $TMPDIR/R.methodsS3_1.6.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/R.methodsS3/R.methodsS3_1.6.1.tar.gz
-R CMD INSTALL $TMPDIR/R.methodsS3_1.6.1.tar.gz
-
-curl -o $TMPDIR/R.oo_1.18.0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/R.oo/R.oo_1.18.0.tar.gz
-R CMD INSTALL $TMPDIR/R.oo_1.18.0.tar.gz
-
-curl -o $TMPDIR/R.utils_1.29.8.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/R.utils/R.utils_1.29.8.tar.gz
-R CMD INSTALL $TMPDIR/R.utils_1.29.8.tar.gz
-
-curl -o $TMPDIR/SDMTools_1.1-20.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/SDMTools/SDMTools_1.1-20.tar.gz
-R CMD INSTALL $TMPDIR/SDMTools_1.1-20.tar.gz
-
-curl -o $TMPDIR/abind_1.4-0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/abind/abind_1.4-0.tar.gz
-R CMD INSTALL $TMPDIR/abind_1.4-0.tar.gz
-
-curl -o $TMPDIR/RColorBrewer_1.0-5.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/RColorBrewer/RColorBrewer_1.0-5.tar.gz
-R CMD INSTALL $TMPDIR/RColorBrewer_1.0-5.tar.gz
-
-curl -o $TMPDIR/latticeExtra_0.6-26.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/latticeExtra_0.6-26.tar.gz
-R CMD INSTALL $TMPDIR/latticeExtra_0.6-26.tar.gz
-
-curl -o $TMPDIR/hexbin_1.26.3.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/hexbin/hexbin_1.26.3.tar.gz
-R CMD INSTALL $TMPDIR/hexbin_1.26.3.tar.gz
-
-curl -o $TMPDIR/zoo_1.7-11.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/zoo/zoo_1.7-11.tar.gz
-R CMD INSTALL $TMPDIR/zoo_1.7-11.tar.gz
-
-curl -o $TMPDIR/rasterVis_0.28.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/rasterVis/rasterVis_0.28.tar.gz
-R CMD INSTALL $TMPDIR/rasterVis_0.28.tar.gz
-
-curl -o $TMPDIR/mda_0.4-4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/mda/mda_0.4-4.tar.gz
-R CMD INSTALL $TMPDIR/mda_0.4-4.tar.gz
-
-curl -o $TMPDIR/randomForest_4.6-7.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/randomForest/randomForest_4.6-7.tar.gz
-R CMD INSTALL $TMPDIR/randomForest_4.6-7.tar.gz
-
-curl -o $TMPDIR/biomod2_3.1-25.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/biomod2/biomod2_3.1-25.tar.gz
-R CMD INSTALL $TMPDIR/biomod2_3.1-25.tar.gz
-
-curl -o $TMPDIR/nlme_3.1-117.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/nlme/nlme_3.1-117.tar.gz
-R CMD INSTALL $TMPDIR/nlme_3.1-117.tar.gz
-
-curl -o $TMPDIR/MASS_7.3-33.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/MASS/MASS_7.3-33.tar.gz
-R CMD INSTALL $TMPDIR/MASS_7.3-33.tar.gz
-
-curl -o $TMPDIR/gamlss.dist_4.3-0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gamlss.dist/gamlss.dist_4.3-0.tar.gz
-R CMD INSTALL $TMPDIR/gamlss.dist_4.3-0.tar.gz
-
-curl -o $TMPDIR/gamlss.data_4.2-7.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gamlss.data/gamlss.data_4.2-7.tar.gz
-R CMD INSTALL $TMPDIR/gamlss.data_4.2-7.tar.gz
-
-curl -o $TMPDIR/gamlss_4.3-0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gamlss/gamlss_4.3-0.tar.gz
-R CMD INSTALL $TMPDIR/gamlss_4.3-0.tar.gz
-
-curl -o $TMPDIR/intervals_0.15.0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/intervals/intervals_0.15.0.tar.gz
-R CMD INSTALL $TMPDIR/intervals_0.15.0.tar.gz
-
-curl -o $TMPDIR/xts_0.9-7.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/xts_0.9-7.tar.gz
-R CMD INSTALL $TMPDIR/xts_0.9-7.tar.gz
-
-curl -o $TMPDIR/FNN_1.1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/FNN_1.1.tar.gz
-R CMD INSTALL $TMPDIR/FNN_1.1.tar.gz
-
-curl -o $TMPDIR/spacetime_1.1-1.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/spacetime/spacetime_1.1-1.tar.gz
-R CMD INSTALL $TMPDIR/spacetime_1.1-1.tar.gz
-
-curl -o $TMPDIR/gstat_1.0-19.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/gstat/gstat_1.0-19.tar.gz
-R CMD INSTALL $TMPDIR/gstat_1.0-19.tar.gz
-
-curl -o $TMPDIR/deldir_0.1-6.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/deldir/deldir_0.1-6.tar.gz
-R CMD INSTALL $TMPDIR/deldir_0.1-6.tar.gz
-
-curl -o $TMPDIR/digest_0.6.4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/digest/digest_0.6.4.tar.gz
-R CMD INSTALL $TMPDIR/digest_0.6.4.tar.gz
-
-curl -o $TMPDIR/gtable_0.1.2.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/gtable_0.1.2.tar.gz
-R CMD INSTALL $TMPDIR/gtable_0.1.2.tar.gz
-
-curl -o $TMPDIR/reshape2_1.4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/reshape2/reshape2_1.4.tar.gz
-R CMD INSTALL $TMPDIR/reshape2_1.4.tar.gz
-
-curl -o $TMPDIR/dichromat_2.0-0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/dichromat_2.0-0.tar.gz
-R CMD INSTALL $TMPDIR/dichromat_2.0-0.tar.gz
-
-curl -o $TMPDIR/colorspace_1.2-4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/colorspace/colorspace_1.2-4.tar.gz
-R CMD INSTALL $TMPDIR/colorspace_1.2-4.tar.gz
-
-curl -o $TMPDIR/munsell_0.4.2.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/munsell_0.4.2.tar.gz
-R CMD INSTALL $TMPDIR/munsell_0.4.2.tar.gz
-
-curl -o $TMPDIR/labeling_0.3.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/labeling_0.3.tar.gz
-R CMD INSTALL $TMPDIR/labeling_0.3.tar.gz
-
-curl -o $TMPDIR/scales_0.2.4.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/scales/scales_0.2.4.tar.gz
-R CMD INSTALL $TMPDIR/scales_0.2.4.tar.gz
-
-curl -o $TMPDIR/proto_0.3-10.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/proto_0.3-10.tar.gz
-R CMD INSTALL $TMPDIR/proto_0.3-10.tar.gz
-
-curl -o $TMPDIR/ggplot2_1.0.0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/Archive/ggplot2/ggplot2_1.0.0.tar.gz
-R CMD INSTALL $TMPDIR/ggplot2_1.0.0.tar.gz
-
-curl -o $TMPDIR/gridExtra_2.0.0.tar.gz http://mirror.aarnet.edu.au/pub/CRAN/src/contrib/gridExtra_2.0.0.tar.gz
-R CMD INSTALL $TMPDIR/gridExtra_2.0.0.tar.gz
-
-rm -rf $TMPDIR
