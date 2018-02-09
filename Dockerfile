709b9ec94c328ee541bbfbfe1085e107267b54ae
Gerhard Weis <g.weis@griffith.edu.au>
2018-02-09 10:35:02 +1000

    update to use latest stable R packages

--- Dockerfile
+++ Dockerfile
@@ -12,45 +12,44 @@ ENV LC_ALL=en_US.UTF-8
 
 # TODO: can I get gdal2 here as well?
 RUN yum install -y http://yum.postgresql.org/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm \
-    && yum install -y \
-    dos2unix \
-    expat-devel \
-    gcc \
-    gcc-c++ \
-    geos \
-    geos-devel \
-    libcurl-devel \
-    libpng \
-    libpng-devel \
-    make \
-    openssl-devel \
-    patch \
-    perl \
-    perl-CPAN \
-    proj proj-devel \
-    proj-epsg \
-    proj-nad \
-    python-devel \
-    R \
-    && yum clean all \
-    && curl https://bootstrap.pypa.io/get-pip.py | python -
+ && yum install -y \
+        dos2unix \
+        expat-devel \
+        gcc \
+        gcc-c++ \
+        geos \
+        geos-devel \
+        libcurl-devel \
+        libpng \
+        libpng-devel \
+        make \
+        openssl-devel \
+        patch \
+        perl \
+        perl-CPAN \
+        proj proj-devel \
+        proj-epsg \
+        proj-nad \
+        python-devel \
+        R \
+ && yum clean all \
+ && curl https://bootstrap.pypa.io/get-pip.py | python -
 
 # Manually Install GDAL and newer version of numpy
 # libarmadillo adds a a number of dependencies: arpack-devel, atlas-devel, blas-devel, lapack-devel, libquadmath-devel, gcc-fortran
-RUN export PIP_INDEX_URL=${PIP_INDEX_URL} && \
-    export PIP_TRUSTED_HOST=${PIP_TRUSTED_HOST} && \
-    export PIP_NO_CACHE_DIR=False && \
-    export PIP_PRE=${PIP_PRE} && \
-    export GDAL_VERSION="2.2.3" && \
-    pip install --no-cache-dir virtualenv guscmversion numpy && \
-    `# install build edpendencies:` && \
-    yum install -y \
+RUN export PIP_INDEX_URL=${PIP_INDEX_URL} \
+ && export PIP_TRUSTED_HOST=${PIP_TRUSTED_HOST} \
+ && export PIP_NO_CACHE_DIR=False \
+ && export PIP_PRE=${PIP_PRE} \
+ && export GDAL_VERSION="2.2.3" \
+ && pip install --no-cache-dir virtualenv setuptools_scm numpy \
+ && `# install build edpendencies:` \
+ && yum install -y \
         make \
         qhull \
         hdf \
-        crytpopp && \
-
-    buildDeps='hdf5-devel \
+        crytpopp \
+ && buildDeps='hdf5-devel \
                libcurl-devel \
                geos-devel \
                xerces-c-devel \
@@ -67,25 +66,22 @@ RUN export PIP_INDEX_URL=${PIP_INDEX_URL} && \
                libdap-devel \
                json-c-devel \
                qhull-devel \
-    ' && \
-
-    yum install -y $buildDeps && \
-
-    cd /tmp && \
-    curl http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz | tar -xz && \
-    cd gdal-${GDAL_VERSION} && \
-    ./configure --with-armadillo --with-poppler --with-epsilon --with-webp --with-cfitsio --with-liblzma=yes --with-dods-root=/usr && \
-    make -j 4 && \
-    make install && \
-    cd .. && \
-
-    `# make sure library paths are set up correctly` && \
-    echo -e '/usr/local/lib\n/usr/local/lib64' > /etc/ld.so.conf.d/local.conf && \
-    ldconfig && \
-    rm -fr gdal-${GDAL_VERSION} && \
-    pip install --no-cache-dir gdal==${GDAL_VERSION} && \
-    yum remove -y $buildDeps && \
-    yum clean all
+    ' \
+ && yum install -y $buildDeps \
+ && cd /tmp \
+ && curl http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION}.tar.gz | tar -xz \
+ && cd gdal-${GDAL_VERSION} \
+ && ./configure --with-armadillo --with-poppler --with-epsilon --with-webp --with-cfitsio --with-liblzma=yes --with-dods-root=/usr \
+ && make -j 4 \
+ && make install \
+ && cd .. \
+ && `# make sure library paths are set up correctly` \
+ && echo -e '/usr/local/lib\n/usr/local/lib64' > /etc/ld.so.conf.d/local.conf \
+ && ldconfig \
+ && rm -fr gdal-${GDAL_VERSION} \
+ && pip install --no-cache-dir gdal==${GDAL_VERSION} \
+ && yum remove -y $buildDeps \
+ && yum clean all
 
 
 # Install everything biodiverse related
@@ -95,36 +91,27 @@ ENV PERL_MM_USE_DEFAULT=1
 
 # TODO: there is a problem with the tests in Geo::GDAL-2.010301 ...
 #       it has been fixed upstream but not released yet.
-RUN set -x && \
-    buildDeps='expat-devel' && \
-    yum install -y $buildDeps && \
-    cpan App::cpanminus && \
-    cpanm YAML::Syck && \
-    cpanm Getopt::Long::Descriptive && \
-    cpanm JSON && \
-    cpanm Time::HiRes@1.9733 && \
-    cpanm --force Object::InsideOut && \
-    cpanm Math::Random::MT::Auto && \
-    cpanm List::BinarySearch && \
-    cpanm List::BinarySearch::XS && \
-    cpanm --force Geo::GDAL && \
-    cpanm Task::Biodiverse::NoGUI && \
-    rm -rf /root/.cpanm && \
-    yum remove -y $buildDeps && \
-    yum clean all
-
-# patch biodiverse
-COPY ./files/biodiverse-1.0-Readonly.patch /opt/
-RUN curl https://codeload.github.com/shawnlaffan/biodiverse/tar.gz/r1.0 | tar xvz -C /opt/ && \
-    dos2unix /opt/biodiverse-r1.0/lib/Biodiverse/Metadata/Indices.pm && \
-    patch -d /opt/biodiverse-r1.0 -p 1 < /opt/biodiverse-1.0-Readonly.patch && \
-    unix2dos /opt/biodiverse-r1.0/lib/Biodiverse/Metadata/Indices.pm
-
-ENV PERL5LIB="/opt/biodiverse-r1.0/lib"
+RUN set -x \
+ && buildDeps='expat-devel' \
+ && yum install -y $buildDeps \
+ && cpan App::cpanminus \
+ && cpanm YAML::Syck \
+ && cpanm Getopt::Long::Descriptive \
+ && cpanm JSON \
+ && cpanm Time::HiRes@1.9733 \
+ && cpanm --force Object::InsideOut \
+ && cpanm Math::Random::MT::Auto \
+ && cpanm List::BinarySearch \
+ && cpanm List::BinarySearch::XS \
+ && cpanm --force Geo::GDAL \
+ && cpanm Task::Biodiverse::NoGUI \
+ && rm -rf /root/.cpanm \
+ && yum remove -y $buildDeps \
+ && yum clean all
 
 # install maxent
 ENV MAXENT=/opt/maxent/maxent.jar
-COPY ./files/maxent.jar ${MAXENT}
+COPY ./files/maxent-3.4.1.jar ${MAXENT}
 
 # Install R libs
 COPY ./files/install_r_packages.sh /tmp/
@@ -134,6 +121,8 @@ RUN yum install -y \
         R-devel \
         libcurl-devel \
         geos-devel \
-        libssh2-devel && \
-    echo 'options(repos=structure(c(CRAN="https://cran.csiro.au")))' >> /root/.Rprofile && \
-    /tmp/install_r_packages.sh
+        libssh2-devel \
+        libxml2-devel \
+        R-devel \
+ && echo 'options(repos=structure(c(CRAN="https://cran.csiro.au")))' >> /root/.Rprofile \
+ && /tmp/install_r_packages.sh
--- files/install_r_packages.sh
+++ files/install_r_packages.sh
@@ -1,120 +1,147 @@
 #! /usr/bin/env Rscript
 
-# require devtools for install_version command
-install.packages('devtools')  # install devtools and all it's dependencies
-require('devtools')
 
-# define list of packages to install
 pkglist = c(
-    'FNN',           '1.1',
-    'KernSmooth',    '2.23-15',
-    'MASS',          '7.3-45',
-    'Matrix',        '1.2-3',
-    'MatrixModels',  '0.4-1',
-    'ModelMetrics',  '1.1.0',
-    'R.methodsS3',   '1.7.1',
-    'R.oo',          '1.20.0',
-    'R.utils',       '2.2.0',
-    'R2HTML',        '2.3.1',
-    'RColorBrewer',  '1.1-2',
-    'Rcpp',          '0.12.3',
-    'RcppEigen',     '0.3.3.3.0',
-    'SDMTools',      '1.1-221',
-    'SparseM',       '1.77',
-    'abind',         '1.4-3',
-    'biomod2',       '3.1-64',
-    'boot',          '1.3-17',
-    'caret',         '6.0-76',
-    'car',           '2.1-0',
-    'class',         '7.3-14',
-    'cluster',       '2.0.3',
-    'codetools',     '0.2-14',
-    'colorspace',    '1.2-6',
-    'deldir',        '0.1-9',
-    'dichromat',     '2.0-0',
-    'digest',        '0.6.9',
-    'dismo',         '1.1-1',
-    'doParallel',    '1.0.10',
-    'evaluate',      '0.8',
-    'foreach',       '1.4.3',
-    'foreign',       '0.8-66',
-    'gam',           '1.12',
-    'gamlss',        '4.3-8',
-    'gamlss.data',   '4.3-2',
-    'gamlss.dist',   '4.3-5',
-    'gbm',           '2.1.1',
-    'gdalUtils',     '2.0.1.7',
-    'ggdendro',      '0.1-20',
-    'ggplot2',       '2.2.1',
-    'gridExtra',     '2.2.0',
-    'gstat',         '1.1-2',
-    'gtable',        '0.2.0',
-    'hexbin',        '1.27.1',
-    'intervals',     '0.15.1',
-    'iterators',     '1.0.8',
-    'labeling',      '0.3',
-    'lazyeval',      '0.2.0',
-    'lattice',       '0.20-33',
-    'latticeExtra',  '0.6-28',
-    'lme4',          '1.1-11',
-    'magrittr',      '1.5',
-    'mda',           '0.4-8',
-    'mgcv',          '1.8-9',
-    'minqa',         '1.2.4',
-    'mmap',          '0.6-12',
-    'munsell',       '0.4.3',
-    'nlme',          '3.1-123',
-    'nloptr',        '1.0.4',
-    'nnet',          '7.3-11',
-    'ordinal',       '2015.1-21',
-    'pbkrtest',      '0.4-2',
-    'pROC',          '1.8',
-    'plyr',          '1.8.3',
-    'png',           '0.1-7',
-    'proto',         '0.3-10',
-    'quantreg',      '5.33',
-    'randomForest',  '4.6-12',
-    'raster',        '2.5-8',
-    'rasterVis',     '0.37',
-    'reshape',       '0.8.5',
-    'reshape2',      '1.4.1',
-    'rgdal',         '1.2-7',
-    'rgeos',         '0.3-23',
-    'rjson',         '0.2.15',
-    'rpart',         '4.1-10',
-    'scales',        '0.4.1',
-    'sp',            '1.2-4',
-    'spacetime',     '1.1-5',
-    'spatial',       '7.3-11',
-    'spatial.tools', '1.4.8',
-    'stringi',       '1.0-1',
-    'stringr',       '1.0.0',
-    'survival',      '2.38-3',
-    'tibble',        '1.3.0',
-    'ucminf',        '1.1-4',
-    'xts',           '0.9-7',
-    'zoo',           '1.7-12'
+    'biomod2',
+    'caret',
+    'dismo',
+    'dplyr',
+    'gam',
+    'gamlss',
+    'gdalUtils',
+    'ggdendro',
+    'gridExtra',
+    'gstat',
+    'jpeg',
+    'lme4',
+    'mgcv',
+    'ordinal',
+    'png',
+    'proj4',
+    'rasterVis',
+    'rgdal',
+    'rgeos',
+    'RCurl',
+    'rjson',
+    'SDMTools',
+    'spatial.tools'
 )
 
-# install method
-install_package <- function(pkg) {
-    installed = installed.packages()
-    if (is.element(pkg['pkg'], installed[,1])) {
-        if (pkg['ver'] != installed[pkg['pkg'], 'Version']) {
-            # already installed
-            return;
-        }
-    }
-    install_version(package=pkg['pkg'], version=pkg['ver'], INSTALL_opts=c('--no-html', '--no-docs', '--clean'))
-}
+# require devtools for install_version command
+# install.packages('devtools')  # install devtools and all it's dependencies
+# require('devtools')
 
-# create package matrix and install packages
-pkgnames = pkglist[seq(1, length(pkglist), 2)]
-packages = matrix(pkglist, ncol=2, nrow=length(pkgnames), byrow=TRUE,
-                  dimnames=list(pkgnames, c('pkg', 'ver')))
+# # define list of packages to install
+# pkglist = c(
+#     # 'FNN',           '1.1',
+#     # 'KernSmooth',    '2.23-15',
+#     # 'MASS',          '7.3-47',
+#     # 'Matrix',        '1.2-12',
+#     'MatrixModels',  '0.4-1',
+#     # 'ModelMetrics',  '1.1.0',
+#     # 'R.methodsS3',   '1.7.1',
+#     # 'R.oo',          '1.21.0',
+#     # 'R.utils',       '2.6.0',
+#     # 'R2HTML',        '2.3.2',
+#     # 'RColorBrewer',  '1.1-2',
+#     # 'Rcpp',          '0.12.14',
+#     # 'RcppEigen',     '0.3.3.3.1',
+#     # 'SDMTools',      '1.1-221',
+#     # 'SparseM',       '1.77',
+#     # 'abind',         '1.4-5',
+#     # 'biomod2',       '3.3-7',
+#     # 'boot',          '1.3-20',
+#     'car',           '2.1-6',
+#     # 'caret',         '6.0-78',
+#     # 'class',         '7.3-14',
+#     # 'cluster',       '2.0.6',
+#     # 'codetools',     '0.2-15',
+#     # 'colorspace',    '1.3-2',
+#     'deldir',        '0.1-14',
+#     # 'dichromat',     '2.0-0',
+#     # 'digest',        '0.6.13',
+#     # 'dismo',         '1.1-4',
+#     # 'doParallel',    '1.0.11',
+#     'evaluate',      '0.10.1',
+#     # 'foreach',       '1.4.4',
+#     # 'foreign',       '0.8-69',
+#     # 'gam',           '1.14-4',
+#     # 'gamlss',        '5.0-6',
+#     # 'gamlss.data',   '5.0-0',
+#     # 'gamlss.dist',   '5.0-4',
+#     # 'gbm',           '2.1.3',
+#     # 'gdalUtils',     '2.0.1.7',
+#     # 'ggdendro',      '0.1-20',
+#     # 'ggplot2',       '2.2.1',
+#     # 'gridExtra',     '2.3',
+#     # 'gstat',         '1.1-5',
+#     # 'gtable',        '0.2.0',
+#     # 'hexbin',        '1.27.1',
+#     # 'intervals',     '0.15.1',
+#     # 'iterators',     '1.0.9',
+#     # 'labeling',      '0.3',
+#     # 'lazyeval',      '0.2.1',
+#     # 'lattice',       '0.20-33',
+#     # 'latticeExtra',  '0.6-28',
+#     # 'lme4',          '1.1-14',
+#     # 'magrittr',      '1.5',
+#     # 'mda',           '0.4-10',
+#     # 'mgcv',          '1.8-22',
+#     # 'minqa',         '1.2.4',
+#     # 'mmap',          '0.6-15',
+#     # 'munsell',       '0.4.3',
+#     # 'nlme',          '3.1-131',
+#     # 'nloptr',        '1.0.4',
+#     # 'nnet',          '7.3-12',
+#     # 'ordinal',       '2015.6-28',
+#     'pbkrtest',      '0.4-7',
+#     # 'pROC',          '1.10.0',
+#     # 'plyr',          '1.8.3',
+#     # 'png',           '0.1-7',
+#     'proto',         '1.0.0',
+#     'quantreg',      '5.34',
+#     # 'randomForest',  '4.6-12',
+#     # 'raster',        '2.6-7',
+#     # 'rasterVis',     '0.41',
+#     # 'reshape',       '0.8.7',
+#     # 'reshape2',      '1.4.3',
+#     # 'rgdal',         '1.2-16',
+#     # 'rgeos',         '0.3-26',
+#     # 'rjson',         '0.2.15',
+#     # 'rpart',         '4.1-11',
+#     # 'scales',        '0.5.0',
+#     # 'sp',            '1.2-5',
+#     # 'spacetime',     '1.2-1',
+#     # 'spatial',       '7.3-11',
+#     # 'spatial.tools', '1.4.8',
+#     # 'stringi',       '1.1.6',
+#     # 'stringr',       '1.2.0',
+#     # 'survival',      '2.41-3',
+#     # 'tibble',        '1.3.4',
+#     # 'ucminf',        '1.1-4',
+#     # 'xts',           '0.10-0',
+#     # 'zoo',           '1.8-0'
+# )
 
-# apply install method to packages data frame
-apply(packages, 1, install_package)
+# # install method
+# install_package <- function(pkg) {
+#     installed = installed.packages()
+#     if (is.element(pkg['pkg'], installed[,1])) {
+#         if (pkg['ver'] != installed[pkg['pkg'], 'Version']) {
+#             # already installed
+#             return;
+#         }
+#     }
+#     install_version(package=pkg['pkg'], version=pkg['ver'], INSTALL_opts=c('--no-html', '--no-docs', '--clean'))
+# }
+
+# # create package matrix and install packages
+# pkgnames = pkglist[seq(1, length(pkglist), 2)]
+# packages = matrix(pkglist, ncol=2, nrow=length(pkgnames), byrow=TRUE,
+#                   dimnames=list(pkgnames, c('pkg', 'ver')))
+
+# # apply install method to packages data frame
+# apply(packages, 1, install_package)
 
 # function to return list of installed package that are not part of base installation
 nobase_pkglist <- function() {
@@ -132,8 +159,11 @@ print_nobase_pkgs <- function() {
     pkgs = nobase_pkglist()
     pkgstrings = c()
     for (name in sort(row.names(pkgs))) {
-        append(pkgstrings,
+        pkgstrings <- append(pkgstrings,
                sprintf("    %-15s '%s'", paste("'", name , "',", sep=""), pkgs[name, 'Version']))
     }
     cat(paste(pkgstrings, sep=",\n"))
 }
+
+install.packages(pkglist)
+print_nobase_pkgs()
